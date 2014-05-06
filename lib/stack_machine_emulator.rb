a6086a39c0198fad8fbb4b4c23f2113bccd64766
Jamie Monserrate <jamiemonserrate@gmail.com>
2014-05-06 23:37:30 +0530

    jMe - Ability to add two numbers

--- lib/stack_machine_emulator.rb
+++ lib/stack_machine_emulator.rb
@@ -1,5 +1,29 @@
 class StackMachineEmulator
+  def initialize
+    @stack = []
+  end
+
   def evaluate(expression_string)
     return expression_string.to_i if expression_string.size == 1
+
+    expression_string.chars.each do |expression_character|
+      @stack.push expression_character.to_i if is_operand?(expression_character)
+      apply_operator(expression_character) if is_operator?(expression_character)
+    end
+
+    @stack.pop
+  end
+
+  private
+  def apply_operator(operand)
+    @stack.push(@stack.pop.send(operand, @stack.pop))
+  end
+
+  def is_operand?(expression_character)
+    (0..9).collect(&:to_s).include? expression_character
+  end
+
+  def is_operator?(expression_character)
+    ['+'].include? expression_character
   end
 end
--- spec/unit/stack_machine_emulator_spec.rb
+++ spec/unit/stack_machine_emulator_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe StackMachineEmulator do
   context '#evaluate' do
     it 'should be able to evaluate a single operand with no operations' do
-      expect(StackMachineEmulator.new.evaluate("1")).to eq(1)
+      expect(StackMachineEmulator.new.evaluate('1')).to eq(1)
+    end
+
+    it 'should be able to evaluate a simple addition of 2 numbers' do
+      expect(StackMachineEmulator.new.evaluate('11+')).to eq(2)
     end
   end
 end
