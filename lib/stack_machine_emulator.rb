0e52978ebd25a6ee7bb11221df58d61c4a4bf696
Jamie Monserrate <jamiemonserrate@gmail.com>
2014-05-07 00:55:11 +0530

    jMe - Dont need this guard clause anymore. The code suffices.

--- lib/stack_machine_emulator.rb
+++ lib/stack_machine_emulator.rb
@@ -4,8 +4,6 @@ class StackMachineEmulator
   end
 
   def evaluate(expression_string)
-    return expression_string.to_i if expression_string.size == 1
-
     expression_string.chars.each do |expression_character|
       @stack.push Operand.new(expression_character) if Operand.valid?(expression_character)
       @stack.push Operator.new(expression_character).apply(@stack.pop, @stack.pop) if Operator.valid?(expression_character)
