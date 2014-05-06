e0127d87a6899a26a08be007fea5a09cce4dbfa1
Jamie Monserrate <jamiemonserrate@gmail.com>
2014-05-06 23:23:04 +0530

    jMe - First happy path. Single operand with no operations

--- lib/stack_machine_emulator.rb
+++ lib/stack_machine_emulator.rb
@@ -1,3 +1,5 @@
 class StackMachineEmulator
-  
+  def evaluate(expression_string)
+    return expression_string.to_i if expression_string.size == 1
+  end
 end
--- spec/unit/stack_machine_emulator_spec.rb
+++ spec/unit/stack_machine_emulator_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe StackMachineEmulator do
-  it 'should run' do
-    StackMachineEmulator.new
+  context '#evaluate' do
+    it 'should be able to evaluate a single operand with no operations' do
+      expect(StackMachineEmulator.new.evaluate("1")).to eq(1)
+    end
   end
 end
